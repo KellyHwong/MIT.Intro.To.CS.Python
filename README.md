3c13d7655253f112834f09be11b968c0cc3d55dc
KellyHwong <huangkan@hust.edu.cn>
2018-09-15 23:20:36 +0800

    updated ps0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MIT.Intro.To.CS.Python
 MIT.Intro.To.CS.Python: Introduction to Computer Science and Programming in Python on MIT OCW(Open Course Ware)
+
+# 
---
+++ ps0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Kelly Hwong on 2018年09月15日23:20:19
+
+import math
+x = input("Enter number x: ")
+y = input("Enter number y: ")
+x = int(x)
+y = int(y)
+print("x**y = " + str(x ** y))
+print("log(x) = " + str(math.log(x)))
+print("log(x,2) = " + str(math.log(x,2)))
